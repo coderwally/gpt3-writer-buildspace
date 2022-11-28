b2cdef776cfa7c9ef09b560595b00aec8988072d
Coder Wally <97439397+coderwally@users.noreply.github.com>
2022-11-28 11:05:57 +0100

    prettier

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
@@ -16,6 +20,15 @@ const Home = () => {
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
           </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
         </div>
       </div>
       <div className="badge-container grow">
