9359940209d9efc4dda291f82eebfbd3a046b965
Coder Wally <97439397+coderwally@users.noreply.github.com>
2022-12-15 12:02:24 +0100

    updated prompt

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
 import buildspaceLogo from "../assets/buildspace-logo.png";
+import Button from '@mui/material/Button';
+import { Link } from "@mui/material";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
 
   const onUserChangedText = (event) => {
-    console.log(event.target.value);
     setUserInput(event.target.value);
   };
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }  
 
   return (
     <div className="root">
@@ -22,7 +46,6 @@ const Home = () => {
             <h2>type a subject and find matching quotes</h2>
           </div>
         </div>
-        {/* Add this code here*/}
         <div className="prompt-container">
           <textarea
             placeholder="start typing here"
@@ -32,24 +55,34 @@ const Home = () => {
           />
         </div>
         <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
-            <div className="generate">
-              <p>Generate</p>
-            </div>
-          </a>
+          <Button variant="contained" color="success" onClick={callGenerateEndpoint}>
+            Generate
+          </Button>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
           </div>
-        </a>
+        )}        
+      </div>
+      <div className="footer">
+        <Link href="https://buildspace.so/p/build-ai-writing-assistant-gpt3" target="_blank" underline="none">
+          <Button variant="outlined" color="info">
+            A Buildspace project
+          </Button>
+        </Link>
+        <Link href="https://twitter.com/traderwally7" target="_blank" underline="none">
+          <Button variant="outlined" color="info">
+            Built by: coderwally<br/> (a.k.a. @traderwally7) 🐤
+          </Button>
+        </Link>
       </div>
     </div>
   );
